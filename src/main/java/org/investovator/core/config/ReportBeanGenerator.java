69be6a28a8e2ddda4f4c7fbfd15ad808e21ef5bc
Amila Surendra <amilasurendra@gmail.com>
2013-10-03 19:06:20 +0530

    Changed to xpath based bean id renamer. Appends stockid to bean ids and refs

--- src/main/java/org/investovator/core/config/ReportBeanGenerator.java
+++ src/main/java/org/investovator/core/config/ReportBeanGenerator.java
@@ -1,11 +1,15 @@
 package org.investovator.core.config;
 
-import net.sf.saxon.TransformerFactoryImpl;
+import org.xml.sax.SAXException;
 
-import javax.xml.transform.*;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
 
 /**
  * @author Amila Surendra
@@ -19,25 +23,59 @@ public class ReportBeanGenerator {
         this.templateXML = templateFile;
     }
 
+
     public File generateXML(String stockID){
 
-        TransformerFactory factory = TransformerFactoryImpl.newInstance();
-        Source xslt = new StreamSource(new File(getClass().getResource("reportTemplate.xslt").getPath()));
-        Transformer transformer = null;
+        String textToFind = "transactionPriceTimeSeriesReport$Stock";
+        String textToReplace = stockID;
 
+        String filepath = getClass().getResource("input.xml").getPath();
+        String fileToBeSaved = "out.xml";
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = null;
         try {
-            transformer = factory.newTransformer(xslt);
+            docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(filepath);
 
-            Source text = new StreamSource(new File(getClass().getResource("input.xml").getPath()));
-            transformer.transform(text, new StreamResult(new File("output.xml")));
+            XPath xpath = XPathFactory.newInstance().newXPath();
 
 
+            // change ATTRIBUTES
+            String xPathExpressionAttr = "//@id|//@ref";
+            NodeList nodesAttr = (NodeList) xpath.evaluate(xPathExpressionAttr, doc, XPathConstants.NODESET);
+
+            String resultAtt ;
+
+            for(int i=0; i<nodesAttr.getLength(); i++) {
+                resultAtt = nodesAttr.item(i).getTextContent();
+                resultAtt = resultAtt + stockID;
+                nodesAttr.item(i).setTextContent(resultAtt);
+            }
+
+            System.out.println("Everything replaced.");
+
+            // save xml file back
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(fileToBeSaved));
+            transformer.transform(source, result);
+
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SAXException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (TransformerConfigurationException e) {
-            e.printStackTrace();
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (TransformerException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-
 
         return null;
     }
