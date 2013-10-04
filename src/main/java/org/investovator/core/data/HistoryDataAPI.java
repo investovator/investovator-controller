c990cd4bbc1b8ef21ddca5bd68d017b207efd3e5
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-10-04 14:52:38 +0530

    data API completed-partially

--- src/main/java/org/investovator/core/data/HistoryDataAPI.java
+++ src/main/java/org/investovator/core/data/HistoryDataAPI.java
@@ -1,6 +1,7 @@
 package org.investovator.core.org.investovator.data;
 
 import org.investovator.core.org.investovator.data.types.HistoryOrderData;
+import org.investovator.data.excel.HistoryData;
 
 import java.util.Date;
 
@@ -12,5 +13,5 @@ public interface HistoryDataAPI {
 
     public HistoryOrderData[] getTradingData(Date startTime, Date endTime, String stockId );
 
-    public com.investovator.data.HistoryData getOHLCPData(Date day,String stockId);
+    public HistoryData getOHLCPData(Date day,String stockId);
 }
