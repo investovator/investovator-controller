e231824107498b0103963f7553a123bc294590fe
Amila Surendra <amilasurendra@gmail.com>
2013-10-05 18:30:45 +0530

    Added method to create model_xml with global dependencies only.

--- src/main/java/org/investovator/controller/config/ModelGenerator.java
+++ src/main/java/org/investovator/controller/config/ModelGenerator.java
@@ -1,12 +1,24 @@
 package org.investovator.controller.config;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+import java.io.File;
 
 /**
  * @author Amila Surendra
@@ -15,9 +27,11 @@ import javax.xml.xpath.XPathFactory;
 public class ModelGenerator {
 
     XMLParser parser;
+    Document templateDoc;
 
     public ModelGenerator(String templateFile){
         parser = new XMLParser(templateFile);
+        templateDoc = parser.getXMLDocumentModel();;
     }
 
     /**
@@ -28,15 +42,13 @@ public class ModelGenerator {
 
         String[] agentNames;
 
-        Document xmlDoc = parser.getXMLDocumentModel();
-
         XPath xpath = XPathFactory.newInstance().newXPath();
 
         String xPathExpressionAttr = "//agents/agent/@name";
 
         NodeList nodesAttr = null;
         try {
-            nodesAttr = (NodeList) xpath.evaluate(xPathExpressionAttr, xmlDoc, XPathConstants.NODESET);
+            nodesAttr = (NodeList) xpath.evaluate(xPathExpressionAttr, templateDoc, XPathConstants.NODESET);
         } catch (XPathExpressionException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
@@ -48,6 +60,67 @@ public class ModelGenerator {
         }
 
         return agentNames;
+    }
+
+
+    public void createModelConfig(){
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+
+        DocumentBuilder docBuilder = null;
+        try {
+            docBuilder = docFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        // root elements
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("beans");
+        doc.appendChild(rootElement);
+
+
+        addGlobalDependencies(doc,rootElement);
+
+
+        createXML(doc);
+
+    }
+
+    private void addGlobalDependencies(Document doc, Element rootElement){
+        XPath xpath = XPathFactory.newInstance().newXPath();
+
+        String xPathExpressionAttr = "/investovator-config/global-dependencies/*";
+
+        NodeList nodesAttr = null;
+        try {
+            nodesAttr = (NodeList) xpath.evaluate(xPathExpressionAttr, templateDoc, XPathConstants.NODESET);
+
+            for (int i = 0; i < nodesAttr.getLength(); i++) {
+                rootElement.appendChild( doc.importNode(nodesAttr.item(i),true) );
+            }
+
+
+        } catch (XPathExpressionException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+
+    private void createXML(Document doc){
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("out.xml"));
+            transformer.transform(source, result);
+
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
 }
--- src/test/java/org/investovator/controller/config/ModelGeneratorTest.java
+++ src/test/java/org/investovator/controller/config/ModelGeneratorTest.java
@@ -1,5 +1,7 @@
 package org.investovator.controller.config;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -7,13 +9,32 @@ import org.junit.Test;
  * @version $Revision
  */
 public class ModelGeneratorTest {
+
+    ModelGenerator generator;
+
+    @Before
+    public void setUp() throws Exception {
+        generator = new ModelGenerator(ModelGenerator.class.getResource("new_template.xml").getPath());
+
+    }
+
     @Test
     public void testGetSupportedAgentTypes() throws Exception {
-        ModelGenerator generator = new ModelGenerator(ModelGenerator.class.getResource("new_template.xml").getPath());
+
         String[] result = generator.getSupportedAgentTypes();
 
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
     }
+
+    @Test
+    public void testCreateModelConfig() throws Exception{
+
+        generator.createModelConfig();
+
+    }
+
+
+
 }
