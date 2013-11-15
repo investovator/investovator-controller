4cc5dddf014f0922858f53d21f2a45cd0dcfee31
Amila Surendra <amilasurendra@gmail.com>
2013-11-15 14:06:22 +0530

    Changed test according to resource file change.

--- src/test/java/org/investovator/controller/config/ConfigGeneratorTest.java
+++ src/test/java/org/investovator/controller/config/ConfigGeneratorTest.java
@@ -3,12 +3,17 @@ package org.investovator.controller.config;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * @author Amila Surendra
  * @version $Revision
  */
 public class ConfigGeneratorTest {
+
     String[] stocks = {"IBM","GOOG","SAMP"};
+    private static String RESOURCE_DIR_PATH = "src" + File.separator + "test" + File.separator + "java"
+            + File.separator + "resources" + File.separator;
 
     ConfigGenerator generator;
 
@@ -16,10 +21,10 @@ public class ConfigGeneratorTest {
     public void setUp() throws Exception {
         generator = new ConfigGenerator(stocks, System.getProperty("user.home") + "/config");
 
-        generator.setModelTemlpateFile(getClass().getResource("model_template.xml").getPath());
-        generator.setReportTemlpateFile(getClass().getResource("report_template.xml").getPath());
-        generator.setMainTemplateFile(getClass().getResource("main_template.xml").getPath());
-        generator.setSpringBeanConfigTemplate(getClass().getResource("bean-config-template.xml").getPath());
+        generator.setModelTemlpateFile(RESOURCE_DIR_PATH + "model_template.xml");
+        generator.setReportTemlpateFile(RESOURCE_DIR_PATH +"report_template.xml");
+        generator.setMainTemplateFile(RESOURCE_DIR_PATH +"main_template.xml");
+        generator.setSpringBeanConfigTemplate(RESOURCE_DIR_PATH +"bean-config-template.xml");
     }
 
     @Test
@@ -29,6 +34,10 @@ public class ConfigGeneratorTest {
         generator.setInitialPrice(100);
         generator.setNoOfDays(1);
         generator.setSpeedFactor(1);
+
+        generator.addProperties(stocks[0],stocks[0] + "url");
+        generator.addProperties(stocks[1],stocks[1] + "url");
+        generator.addProperties(stocks[2],stocks[2] + "url");
 
         String[] types = generator.getSupportedReports();
         String[] result = generator.getDependencyReportBeans(types[0]);
