836730ed11029204ead0c2c829405a3a9ef7f149
Amila Surendra <amilasurendra@gmail.com>
2013-11-24 10:32:28 +0530

    Added property verify test.

--- src/test/java/org/investovator/controller/config/ModelGeneratorTest.java
+++ src/test/java/org/investovator/controller/config/ModelGeneratorTest.java
@@ -4,8 +4,23 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -49,9 +64,11 @@ public class ModelGeneratorTest {
         generator.addAgent(generator.getSupportedAgentTypes()[0],120);
         generator.addAgent(generator.getSupportedAgentTypes()[1],100);
 
-        ReportGenerator gen = new ReportGenerator(getClass().getResource("report_template.xml").getPath());
-        String[] types = gen.getSupportedReports();
-        String[] result = gen.getDependencyReportBeans(types[0]);
+        ReportGenerator reportGenerator = new ReportGenerator(resourcePath + "report_template.xml");
+        reportGenerator.setOutputTemplateDoc(resourcePath + "bean-config-template.xml");
+        reportGenerator.setOutputPath(tempDir + "out.xml");
+        String[] types = reportGenerator.getSupportedReports();
+        String[] result = reportGenerator.getDependencyReportBeans(types[0]);
 
         generator.addDependencyReportBean(result);
 
@@ -61,6 +78,36 @@ public class ModelGeneratorTest {
         generator.addSimulationProperty("$initialPrice","500");
 
         generator.createModelConfig();
+
+
+        //Validation
+        URL schemaFile = new URL("http://www.springframework.org/schema/beans/spring-beans.xsd");
+        Source xmlFile = new StreamSource(new File(tempDir +"out.xml"));
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(schemaFile);
+        Validator validator = schema.newValidator();
+        try {
+            validator.validate(xmlFile);
+            Assert.assertTrue(true);
+        } catch (SAXException e) {
+            Assert.assertTrue("Generated model bean xml validation fails.",false);
+        }
+
+        //Property Validation
+        XMLParser parser = new XMLParser(tempDir+"out.xml");
+        Document doc = parser.getXMLDocumentModel();
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        Node nodesAttr = null;
+
+        String xPathExpressionAttr = "//property[@name='lengthOfDay']/@value";
+        nodesAttr = (Node) xpath.evaluate(xPathExpressionAttr, doc, XPathConstants.NODE);
+        Assert.assertEquals("200000", nodesAttr.getNodeValue());
+
+        xPathExpressionAttr = "//property[@name='maximumDays']/@value";
+        nodesAttr = (Node) xpath.evaluate(xPathExpressionAttr, doc, XPathConstants.NODE);
+        Assert.assertEquals("1", nodesAttr.getNodeValue());
 
     }
 
