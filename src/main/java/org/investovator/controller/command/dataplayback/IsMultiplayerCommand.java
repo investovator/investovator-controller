cb08311c9e8ca54324bec09e0b4d0f831210e0be
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-11-28 22:52:21 +0530

    Added a new command. Changed a DPE class to suit the new DB

---
+++ src/main/java/org/investovator/controller/command/dataplayback/IsMultiplayerCommand.java
@@ -0,0 +1,44 @@
+/*
+ * investovator, Stock Market Gaming Framework
+ *     Copyright (C) 2013  investovator
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package org.investovator.controller.command.dataplayback;
+
+import org.investovator.controller.command.exception.CommandExecutionException;
+
+/**
+ * @author: ishan
+ * @version: ${Revision}
+ */
+public class IsMultiplayerCommand extends DataPlaybackGameCommand {
+    private boolean multiplayer;
+    @Override
+    public void execute() throws CommandExecutionException {
+        if(player.isMultiplayer()){
+            multiplayer=true;
+        }
+        else{
+            multiplayer=false;
+        }
+
+    }
+
+    public boolean isMultiplayer(){
+        return multiplayer;
+    }
+}
